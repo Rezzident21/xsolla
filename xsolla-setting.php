0a14fd42468a982bc2b91d2efa0bc40a2cb55297
Rezzident <seredyuk.davi@gmail.com>
2021-09-05 15:20:51 +0300

    Create xsolla-setting.php

---
+++ xsolla-setting.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');                # Host;
+define('DB_PORT', '3306');                     # Port;
+define('DB_NAME', 'xsolla');                # Name;
+define('DB_USER', '');                # User;
+define('DB_PASS', '');              # Password;
+define('XSOLLA_CODE', ''); // secret code  
